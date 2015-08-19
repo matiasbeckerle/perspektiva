1b6591055e0ec636a2ebd7442373ec111e4babca
Matias Beckerle <matiasbeckerle@gmail.com>
2015-08-18 21:26:47 -0300

    Bootstrap an empty Unity project

---
+++ source/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles the game. Little task, right?
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Static instance of GameManager which allows it to be accessed by any other script.
+    public static GameManager Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ source/Assets/Scripts/Loader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour
+{
+    // GameManager prefab to instantiate.
+    [SerializeField]
+    GameObject gameManager;
+
+    void Awake()
+    {
+        // Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        if (GameManager.Instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+}
