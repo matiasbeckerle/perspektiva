e1d5cd900f67f2dce51b39e96ca6998391eb2bda
Matias <matiasbeckerle@gmail.com>
2015-08-22 15:44:09 -0300

    Add boundaries, paddle and basic player movement

---
+++ source/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public Boundary boundary;
+    public float speed = 1;
+
+    void Update()
+    {
+        // Capture user input.
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        // Create a new movement based on user input and speed.
+        var movement = new Vector3(horizontalInput, 0, 0);
+        movement = movement * speed;
+
+        // Move the player to it's current position plus the movement.
+        var newPosition = transform.position + movement;
+        newPosition = new Vector3(Mathf.Clamp(newPosition.x, boundary.xMin, boundary.xMax), newPosition.y, newPosition.z);
+        transform.position = newPosition;
+    }
+
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax;
+    }
+}
