20a7e0f5779c9fcce6e58f8faa368251f0d2f09a
Matias Beckerle <matiasbeckerle@gmail.com>
2015-08-24 19:51:38 -0300

    Add dead zone

---
+++ source/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        // TODO: game over using GameManager.
+    }
+}
