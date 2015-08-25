91f6293beedc1f09485dc88862d1e804eb71980c
Matias Beckerle <matiasbeckerle@gmail.com>
2015-08-25 20:31:25 -0300

    Add basic UIManager and basic UI to show current game level

--- source/Assets/Scripts/Loader.cs
+++ source/Assets/Scripts/Loader.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Loader : MonoBehaviour
 {
+    // UIManager prefab to instantiate.
+    [SerializeField]
+    GameObject uiManager;
+
     // GameManager prefab to instantiate.
     [SerializeField]
     GameObject gameManager;
 
     void Awake()
     {
-        // Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        // Check if a UIManager has already been assigned to static variable UIManager.instance or if it's still null.
+        if (UIManager.Instance == null)
+        {
+            Instantiate(uiManager);
+        }
+
+        // GameManager should be the last one to load.
+        // Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null.
         if (GameManager.Instance == null)
         {
             Instantiate(gameManager);
---
+++ source/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Static instance of UIManager which allows it to be accessed by any other script.
+    public static UIManager Instance = null;
+
+    public Text levelText;
+    public GameObject levelImage;
+    public float levelStartDelay = 2f;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ShowLevel(int level)
+    {
+        levelText.text = "Level " + level;
+        levelImage.SetActive(true);
+
+        Invoke("HideLevel", levelStartDelay);
+    }
+
+    public void HideLevel()
+    {
+        levelImage.SetActive(false);
+    }
+}
