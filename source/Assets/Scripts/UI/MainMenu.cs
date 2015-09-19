4cb15557afdfc82e2de19a0a0b03cde6e9388435
Matias Beckerle <matiasbeckerle@gmail.com>
2015-09-19 11:36:15 -0300

    Add MainMenu and remove old UI

---
+++ source/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public static MainMenu Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Shows the InGameUI in the scene.
+    /// </summary>
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Hides the InGameUI from the scene.
+    /// </summary>
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
