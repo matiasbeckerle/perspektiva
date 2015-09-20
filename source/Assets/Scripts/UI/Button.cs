26172e02e2b0ef6dcb6bc8098e4c40fe1cf52e12
Matias Beckerle <matiasbeckerle@gmail.com>
2015-09-20 14:38:36 -0300

    Add more sound effects

---
+++ source/Assets/Scripts/UI/Button.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public AudioClip selectSound;
+    public AudioClip enterSound;
+
+    public void OnSelect()
+    {
+        SoundManager.Instance.PlaySingle(selectSound);
+    }
+
+    public void OnSubmit()
+    {
+        SoundManager.Instance.PlaySingle(enterSound);
+    }
+}
