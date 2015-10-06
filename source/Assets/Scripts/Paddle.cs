25670420f838b4aebb466cc862cfa0a45b43307b
Matias Beckerle <matiasbeckerle@gmail.com>
2015-10-05 21:08:03 -0300

    Rename Player for Paddle

--- source/Assets/Scripts/Paddle.cs
+++ source/Assets/Scripts/Paddle.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
     /// <summary>
     /// Limits for player movements restriction.
