a0a3a2194065e563864e2f40acfb1dfd6c7dc9f0
Matias Beckerle <matiasbeckerle@gmail.com>
2015-10-11 10:48:16 -0300

    Clamp paddle movement to boundaries

--- source/Assets/Scripts/Paddle.cs
+++ source/Assets/Scripts/Paddle.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class Paddle : MonoBehaviour
 {
     /// <summary>
+    /// Horizontal limits to move the paddle.
+    /// </summary>
+    public Boundary boundary;
+
+    /// <summary>
     /// Paddle's speed.
     /// </summary>
     public float speed = 20;
@@ -25,11 +30,20 @@ public class Paddle : MonoBehaviour
 
     protected void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        _movement.Set(h, 0, 0);
+        _movement.Set(horizontalInput, 0, 0);
         _movement = _movement.normalized * speed * Time.deltaTime;
 
-        _rigidbody.MovePosition(transform.position + _movement);
+        Vector3 nextPosition = transform.position + _movement;
+        nextPosition.x = Mathf.Clamp(nextPosition.x, boundary.xMin, boundary.xMax);
+
+        _rigidbody.MovePosition(nextPosition);
+    }
+
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax;
     }
 }
